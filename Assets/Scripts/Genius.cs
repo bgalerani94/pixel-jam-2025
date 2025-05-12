675edf113c697c309bd63aa7891232000228fe86
Felipe Bigatto <38051046+bigatto@users.noreply.github.com>
2025-05-12 17:17:23 -0300

    Created the class GeniusEngine
    Removed the Hello World script, created a new class named Main for better maintenance.
    Created the class "GeniusEngine" to make the code cleaner

--- Assets/Scripts/Genius.cs
+++ Assets/Scripts/Genius.cs
@@ -1,29 +1,22 @@
-using UnityEngine;
-using System.Collections.Generic; // Importa o namespace System.Collections.Generic para usar List<T>
+using System.Collections.Generic; // Necessário para usar List<T>
+using UnityEngine; // Necessário para usar UnityEngine.Random
 
-public class HelloWorld : MonoBehaviour
+// Classe GeniusEngine agora sem herdar de MonoBehaviour
+public class GeniusEngine
 {
-    public float speed = 5.0f; // Speed of the object
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // Método Genius que retorna uma lista de números aleatórios
+    public List<int> Genius()
     {
-        List<int> numeros = new List<int>();
-        int round = 5; //o round que o jogador está jogando define a quantidade de cores que ele pode escolher
+        List<int> numeros = new List<int>(); // Cria a lista para armazenar os números aleatórios
+        int round = 5; // O round define o número de cores/itens que o jogador pode escolher
 
+        // Adiciona números aleatórios de 1 a 4 à lista
+        for (int i = 0; i < round; i++)
+        {
+            // Gera números aleatórios de 1 a 4 (1 = azul, 2 = amarelo, 3 = vermelho, 4 = verde)
+            numeros.Add(UnityEngine.Random.Range(1, 5)); // UnityEngine.Random.Range(min, max) retorna valores entre min (inclusive) e max (exclusivo)
+        }
 
-        // Adiciona números de 1 a 4 à lista de tamanho igual ao round
-        for (int i = 1; i <= round; i++)
-            {
-                // Gera números aleatórios de 1 a 4
-                // 1 = azul, 2 = amarelo, 3 = vermelho, 4 = verde
-                numeros.Add(UnityEngine.Random.Range(1, 5));
-            }
-        Debug.Log("Lista completa: " + string.Join(", ", numeros));
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        transform.position += new Vector3(1.5f, -2, 0) * Time.deltaTime * speed;
+        return numeros; // Retorna a lista com os números aleatórios
     }
 }
---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections.Generic; // Necessário para usar List<T>
+
+public class Main : MonoBehaviour
+{
+    public float speed = 5.0f; // Speed of the object
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        List<int> numeros = new List<int>();
+        GeniusEngine g = new GeniusEngine(); // Cria uma instância da classe GEngine
+        numeros = g.Genius(); // Chama o método Genius da classe GEngine
+        Debug.Log("Lista completa: " + string.Join(", ", numeros));        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(1.5f, -2, 0) * Time.deltaTime * speed;        
+    }
+}
