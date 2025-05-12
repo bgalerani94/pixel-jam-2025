9e905f5295128c721ed38af84a0c03531838c24f
Felipe Bigatto <38051046+bigatto@users.noreply.github.com>
2025-05-12 15:03:02 -0300

    Implementing Genius
    Renaming the script "Hello World" to Genius (can be changed if needed) and implementing the first version of the Genius quizz that'll be used within the game

---
+++ Assets/Scripts/Genius.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections.Generic; // Importa o namespace System.Collections.Generic para usar List<T>
+
+public class HelloWorld : MonoBehaviour
+{
+    public float speed = 5.0f; // Speed of the object
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        List<int> numeros = new List<int>();
+        int round = 5; //o round que o jogador está jogando define a quantidade de cores que ele pode escolher
+
+
+        // Adiciona números de 1 a <ROUND> à lista
+        for (int i = 1; i <= round; i++)
+            {
+                // Gera números aleatórios de 1 a <ROUND>
+                // 1 = azul, 2 = amarelo, 3 = vermelho, 4 = verde
+                numeros.Add(UnityEngine.Random.Range(1, 5));
+            }
+        Debug.Log("Lista completa: " + string.Join(", ", numeros));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(1.5f, -2, 0) * Time.deltaTime * speed;
+    }
+}
