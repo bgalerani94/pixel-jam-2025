909738cfb5f768ec102f6029482de14776db7787
Felipe Bigatto <38051046+bigatto@users.noreply.github.com>
2025-05-13 12:13:19 -0300

    Removing square
    Removing the demo square and it's movement.
    
    Also removing line 10 where List<int> was no longer being used in the main. Keeping the code cleaner

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -2,12 +2,10 @@ using UnityEngine;
 using System.Collections.Generic; // Necessário para usar List<T>
 
 public class Main : MonoBehaviour
-{
-    public float speed = 5.0f; // Speed of the object
+{    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
-        List<int> numeros = new List<int>();
+    {        
         GeniusEngine g = new GeniusEngine(); // Cria uma instância da classe GEngine        
         g.Genius(); // Gera os números aleatórios
     }
@@ -15,6 +13,6 @@ public class Main : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(1.5f, -2, 0) * Time.deltaTime * speed;        
+                
     }
 }
