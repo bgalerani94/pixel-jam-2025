ece867c6c35c33a7dfc19db329f24ec61e519ac4
Felipe Bigatto <38051046+Bigatto@users.noreply.github.com>
2025-05-14 10:49:53 -0300

    Update Genius.cs

--- Assets/Scripts/Genius.cs
+++ Assets/Scripts/Genius.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic; // Necessário para usar List<T>
-using UnityEngine; // Necessário para usar UnityEngine.Random
+using UnityEngine;
 
-// Classe GeniusEngine agora sem herdar de MonoBehaviour
 public class GeniusEngine
 {
     // Método Genius que retorna uma lista de números aleatórios
@@ -12,7 +11,7 @@ public class GeniusEngine
         for (int i = 0; i < 5; i++)
         {
             // Gera números aleatórios de 1 a 4 (1 = azul, 2 = amarelo, 3 = vermelho, 4 = verde)
-            numeros.Add(UnityEngine.Random.Range(1, 5)); // UnityEngine.Random.Range(min, max) retorna valores entre min (inclusive) e max (exclusivo)
+            numeros.Add(UnityEngine.Random.Range(1, 5));
         }
         Debug.Log("Números gerados: " + string.Join(", ", numeros)); // Exibe os números gerados no console
 
