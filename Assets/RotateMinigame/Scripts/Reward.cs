14f7eb1907d052b309761ff43677a9ff85f577bc
Bruno Galerani <brunogalerani@gmail.com>
2025-05-17 23:39:23 -0300

    Improving the reward animation

--- Assets/RotateMinigame/Scripts/Reward.cs
+++ Assets/RotateMinigame/Scripts/Reward.cs
@@ -5,19 +5,16 @@ namespace RotateMinigame.Scripts
 {
     public class Reward : MonoBehaviour
     {
-        [SerializeField] private float yFirstMovementAmount;
         [SerializeField] private float yLoopMovementAmount;
-        [SerializeField] private float firstMovementAnimTime;
+        [SerializeField] private float scaleAnimTime;
         [SerializeField] private float loopAnimTime;
 
         private void OnEnable()
         {
-            transform.DOLocalMoveY(yFirstMovementAmount, firstMovementAnimTime)
+            transform.DOScale(Vector3.one, scaleAnimTime);
+            transform.DOLocalMoveY(transform.localPosition.y + yLoopMovementAmount, loopAnimTime)
                 .SetEase(Ease.InOutCubic)
-                .OnComplete(() =>
-                    transform.DOLocalMoveY(transform.localPosition.y + yLoopMovementAmount, loopAnimTime)
-                        .SetEase(Ease.InOutCubic)
-                        .SetLoops(-1, LoopType.Yoyo));
+                .SetLoops(-1, LoopType.Yoyo);
         }
     }
 }
