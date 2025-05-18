dbeb39aef8f22607830aec80271dd3a65cce9299
Bruno Galerani <brunogalerani@gmail.com>
2025-05-18 17:23:31 -0300

    Adding mute bg sound button

--- Assets/Music/MusicController.cs
+++ Assets/Music/MusicController.cs
@@ -1,48 +1,25 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Music
 {
     public class MusicController : MonoBehaviour
     {
-        #region Singleton
-
-        private static MusicController _instance;
-
-        public static MusicController Instance
-        {
-            get
-            {
-                if (_instance == null)
-                {
-                    _instance = FindAnyObjectByType<MusicController>();
-                    if (_instance == null)
-                    {
-                        _instance = Instantiate(Resources.Load<MusicController>("MusicController"));
-                    }
-
-                    DontDestroyOnLoad(_instance.gameObject);
-                }
-
-                return _instance;
-            }
-        }
-
-        #endregion
-
         [SerializeField] private AudioSource audioSource;
+        [SerializeField] private Button muteButton;
+        [SerializeField] private Sprite muteSprite;
+        [SerializeField] private Sprite unmuteSprite;
 
         private void Awake()
         {
-            if (_instance == null)
-            {
-                _instance = this;
-                DontDestroyOnLoad(gameObject);
-            }
+            DontDestroyOnLoad(gameObject);
+            muteButton.onClick.AddListener(ToggleMute);
         }
 
-        public void SetMusicVolume(float volume)
+        private void ToggleMute()
         {
-            audioSource.volume = volume;
+            audioSource.mute = !audioSource.mute;
+            muteButton.image.sprite = audioSource.mute ? muteSprite : unmuteSprite;
         }
     }
 }
